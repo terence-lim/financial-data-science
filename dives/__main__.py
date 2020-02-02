949b21c60096869722c0cf023ed9d06a4752d4c9
Terence Lim <41972012+terence-lim@users.noreply.github.com>
2020-02-02 17:29:17 -0500

    20200202

--- dives/__main__.py
+++
@@ -1,7 +0,0 @@
-import dives
-if __name__ == '__main__':
-    import importlib
-    importlib.reload(dives)
-    importlib.reload(dives.util)
-    importlib.reload(dives.custom)
-    print('dives/__main__: reload dives')
